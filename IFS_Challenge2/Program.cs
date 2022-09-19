8a1e91b2959bea4b84b23f741f7d5f791995b4fe
aruls <aruls@SHEN-PC>
2022-09-19 00:19:56 -0700

    Add project files.

---
+++ IFS_Challenge2/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int x = 29;
+        int sum = 0;
+
+        for(int i = x; i > 0; i--)
+        {
+            int div = 0;
+            for (int j = i; j > 0; j--)
+            {
+                if (i % j == 0) div++; 
+            }
+            if (div == 2) sum = sum + i;
+        }
+        Console.WriteLine("Sum of Prime numbers below X: " + sum);
+    }
+}
